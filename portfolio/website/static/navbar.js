b6aac5c929e237da99db89658f33c657422e6aa4
Justin <codewithjustin@yahoo.com>
2024-02-23 23:59:11 -0500

    Changes

---
+++ portfolio/website/static/navbar.js
@@ -0,0 +1,13 @@
+// Navbar //
+
+hamburger = document.getElementById('hamburger_menu');
+drop_down_nav = document.getElementById('drop_down_nav');
+
+
+function respnav() {
+    // Add class
+    alert("Responsive Nav Triggered.")
+}
+
+
+hamburger.addEventListener("click", respnav)
